b94839f7222d1e7c1537d84c928c6787e960cc54
Hernan Case <hercase92@gmail.com>
2021-10-27 23:49:37 -0300

    remove styles from pages folder

--- pages/match/[id].js
+++ pages/match/[id].js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import Image from "next/image";
-import { StyledMatch } from "./match.styled";
 import Head from "next/head";
 
 // Services
@@ -13,6 +12,7 @@ import ShareIcon from "components/atoms/Icons/ShareIcon";
 import Feedback from "components/templates/Feedback";
 import PlayersList from "components/molecules/PlayersList";
 import { convertTimestampToDate, upperFirst } from "helpers";
+import styled from "styled-components";
 
 const Match = ({ match }) => {
   const SEO = {
@@ -93,6 +93,51 @@ const Match = ({ match }) => {
 Match.propTypes = {
   match: PropTypes.any,
 };
+
+const StyledMatch = styled.div`
+  & .content {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+  }
+
+  & .header {
+    display: flex;
+    border-radius: var(--radius);
+    overflow: hidden;
+
+    & .icon {
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      justify-content: center;
+      background-color: var(--primary);
+      color: var(--headline);
+      width: 4rem;
+    }
+
+    & .data {
+      flex: 1;
+      background-color: white;
+      font-size: 0.875rem;
+      line-height: 1.25rem;
+      padding: 0.5rem 1rem;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    & .title {
+      color: var(--secondary);
+      font-weight: 600;
+    }
+
+    & .subtitle {
+      font-weight: 500;
+    }
+  }
+`;
 
 export async function getServerSideProps({ req, params }) {
   const match = await getMatchById(params.id);
